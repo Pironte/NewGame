ae566fcf9be17886b3558fbd02cca010f4cfd7f9
João Vitor Ricci Pironte <50086474+Pironte@users.noreply.github.com>
2023-04-15 15:28:31 -0300

    Primeiro Commit do projeto

---
+++ ProjetoFodase2.0/Assets/Pixel Adventure 1/Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rig;
+
+    public bool isJumping;
+    public bool isDoubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                isDoubleJump = true;
+            }
+            else if (isDoubleJump)
+            {
+                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                isDoubleJump = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6)
+        {
+            isJumping = true;
+        }
+    }
+}
